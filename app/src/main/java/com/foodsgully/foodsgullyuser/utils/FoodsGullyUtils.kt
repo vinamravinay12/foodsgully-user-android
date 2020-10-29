acfc26b2b29d6145e41673b38fd791e1c58281e8
vinamravinay-niro <vinamra.vinay235@gmail.com>
2020-10-29 16:53:09 +0530

    added libraires

--- app/src/main/java/com/foodsgully/foodsgullyuser/utils/FoodsGullyUtils.kt
+++ app/src/main/java/com/foodsgully/foodsgullyuser/utils/FoodsGullyUtils.kt
@@ -1,16 +1,39 @@
 package com.foodsgully.foodsgullyuser.utils
 
+import android.app.ProgressDialog
 import android.content.Context
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
+import com.niro.niroapp.database.SharedPreferenceManager
 
 object FoodsGullyUtils {
 
     @JvmStatic
-    fun getToken(context : Context) : String {
-        return ""
+    fun showSnackbar(message: String, root: View) {
+
+        Snackbar.make(root, message, Snackbar.LENGTH_SHORT).show()
     }
 
     @JvmStatic
+    fun showLoaderProgress(message: String, context: Context): ProgressDialog {
+        val progressDialog = ProgressDialog(context)
+        progressDialog.setMessage(message
+        progressDialog.show(
+        return progressDialog
+    }
+
+    @JvmStatic
+    fun getToken(context: Context): String? {
+        return "Bearer " + SharedPreferenceManager(
+            context,
+            NiroAppConstants.LOGIN_SP
+        ).getStringPreference(NiroAppConstants.USER_TOKEN)
+    }
+
+
+    @JvmStatic
     fun getDefaultErrorMessage(context: Context?): String {
-        return ""
+
+        return context?.getString(R.string.something_went_wrong) ?: NiroAppConstants.SWW
     }
 }
