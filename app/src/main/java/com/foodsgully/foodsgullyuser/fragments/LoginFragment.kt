4ad1a8d8598b7380b70b880a9092b58387b5b0d4
vinamravinay-niro <vinamra.vinay235@gmail.com>
2020-10-29 11:36:23 +0530

    Added Login Screen

--- app/src/main/java/com/foodsgully/foodsgullyuser/fragments/LoginFragment.kt
+++ app/src/main/java/com/foodsgully/foodsgullyuser/fragments/LoginFragment.kt
@@ -1,33 +1,72 @@
 package com.foodsgully.foodsgullyuser.fragments
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.foodsgully.foodsgullyuser.viewmodels.LoginViewModel
 import com.niro.foodsgullyuser.R
+import com.niro.foodsgullyuser.databinding.LoginFragmentBinding
 
 class LoginFragment : Fragment() {
+
+    private lateinit var bindingLoginFragment : LoginFragmentBinding
+    private var loginViewModel : LoginViewModel? = null
 
     companion object {
         fun newInstance() = LoginFragment()
     }
 
-    private lateinit var viewModel: LoginViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.login_fragment, container, false)
+        bindingLoginFragment = DataBindingUtil.inflate(inflater,R.layout.login_fragment,container,false)
+        bindingLoginFragment.lifecycleOwner = viewLifecycleOwner
+        return bindingLoginFragment.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(LoginViewModel::class.java)
-        // TODO: Use the ViewModel
+        loginViewModel = ViewModelProvider(requireActivity()).get(LoginViewModel::class.java)
+
+        bindingLoginFragment.loginVM = loginViewModel
+
+        initializeListeners()
+    }
+
+    private fun initializeListeners() {
+        initializeFocusChangeListeners()
+        initializeClickListeners()
+    }
+
+    private fun initializeFocusChangeListeners() {
+        bindingLoginFragment.etEmail.setOnFocusChangeListener { v, hasFocus -> if(!hasFocus) loginViewModel?.validateEmail(requireContext()) }
+
+        bindingLoginFragment.etPassword.setOnFocusChangeListener { v, hasFocus -> if(!hasFocus) loginViewModel?.validatePassword(requireContext()) }
+    }
+
+    private fun initializeClickListeners() {
+        bindingLoginFragment.btnLogin.setOnClickListener { handleUserLogin() }
+        bindingLoginFragment.tvSignup.setOnClickListener{ launchSignupScreen() }
+        bindingLoginFragment.tvForgotPassword.setOnClickListener { launchForgotPasswordScreen() }
+
+    }
+
+    private fun launchForgotPasswordScreen() {
+
+    }
+
+    private fun launchSignupScreen() {
+
+    }
+
+    private fun handleUserLogin() {
+
     }
 
 }
--- app/src/main/java/com/foodsgully/foodsgullyuser/viewmodels/LoginViewModel.kt
+++ app/src/main/java/com/foodsgully/foodsgullyuser/viewmodels/LoginViewModel.kt
@@ -1,7 +1,63 @@
 package com.foodsgully.foodsgullyuser.viewmodels
 
+import android.content.Context
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.niro.foodsgullyuser.R
+import com.wajahatkarim3.easyvalidation.core.view_ktx.validEmail
+
 
 class LoginViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private var email = MutableLiveData<String>()
+    private var password = MutableLiveData<String>()
+
+    private var emailError = MutableLiveData<String>()
+    private var passwordError = MutableLiveData<String>()
+
+
+    fun getEmail() = email
+    fun getPassword() = password
+    fun getEmailError() = emailError
+    fun getPasswordError() = passwordError
+    fun validateEmail(context: Context)  : Boolean {
+
+        return when {
+            email.value.isNullOrEmpty() -> {
+                emailError.value = context.getString(R.string.error_empty_email)
+                false
+            }
+            email.value?.validEmail() == false -> {
+                emailError.value = context.getString(R.string.invalid_email)
+                false
+            }
+            else -> {
+                emailError.value = ""
+                true
+            }
+        }
+    }
+
+    fun validatePassword(context: Context) : Boolean {
+        when {
+            password.value.isNullOrEmpty() -> {
+                passwordError.value = context.getString(R.string.error_empty_password)
+                return false
+            }
+
+            else -> passwordError.value = ""
+        }
+        return true
+
+    }
+
+
+    fun validateLogin(context: Context) : Boolean {
+        return validateEmail(context) && validatePassword(context)
+    }
+
+    fun loginUser() {
+
+    }
+
 }
