432eee7df4f169712515eeeb6fa063b8fe0e1ef8
vinamravinay-niro <vinamra.vinay235@gmail.com>
2020-10-29 16:28:34 +0530

    Added GenericResponse

---
+++ app/src/main/java/com/foodsgully/foodsgullyuser/models/APIResponse.kt
@@ -0,0 +1,9 @@
+package com.foodsgully.foodsgullyuser.models
+
+sealed class APIResponse
+
+data class APIError( val errorCode : Int,  val errorMessage : String) : APIResponse()
+
+data class Success<T>( val data : T? ) : APIResponse()
+
+data class APILoader( val isLoading : Boolean) : APIResponse()
